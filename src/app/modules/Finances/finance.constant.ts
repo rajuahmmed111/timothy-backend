0ca0e53ea4800e91a64882b88c9c500c0cca485d
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-09 00:37:13 +0600

    add two file in finance

---
+++ src/app/modules/Finances/finance.constant.ts
@@ -0,0 +1,16 @@
+export const filterField: string[] = [
+  "searchTerm",
+  "fullName",
+  "email",
+  "contactNumber",
+  "country",
+  "status",
+  "timeRange",
+];
+export const searchableFields: string[] = [
+  "fullName",
+  "email",
+  "contactNumber",
+  "country",
+  "status",
+];
---
+++ src/app/modules/Finances/finance.interface.ts
@@ -0,0 +1,9 @@
+export type IFilterRequest = {
+  searchTerm?: string | undefined;
+  fullName?: string | undefined;
+  email?: string | undefined;
+  contactNumber?: string | undefined;
+  country?: string | undefined;
+  status?: string | undefined;
+  timeRange?: string | undefined;
+};
