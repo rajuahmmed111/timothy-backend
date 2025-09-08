7c3fdda83e75f928a7ef86766c393f2537985afb
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-09 05:02:11 +0600

    get overview

--- src/app/modules/Statistics/statistics.route.ts
+++ src/app/modules/Statistics/statistics.route.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // get overview total user, total partner,total contracts , admin earnings
 router.get(
-  "/",
+  "/overview",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   StatisticsController.getOverview
 );
