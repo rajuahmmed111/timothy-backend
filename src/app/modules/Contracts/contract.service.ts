03d62b2c88a13c0663b54c0e468f52198e8e1517
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-09 05:07:45 +0600

    get all contractrio

--- src/app/modules/Contracts/contract.service.ts
+++ src/app/modules/Contracts/contract.service.ts
@@ -3,19 +3,19 @@ import prisma from "../../../shared/prisma";
 
 // get all contracts (bookings)
 const getAllContracts = async () => {
-    // find all hotel bookings
-    const result = await prisma.hotel_Booking.findMany();
+  // find all hotel bookings
+  const hotel = await prisma.hotel_Booking.findMany();
 
-    // find all security bookings
-    const result2 = await prisma.security_Booking.findMany();
+  // find all security bookings
+  const security = await prisma.security_Booking.findMany();
 
-    // find all car bookings
-    const result3 = await prisma.car_Booking.findMany();
+  // find all car bookings
+  const car = await prisma.car_Booking.findMany();
 
-    // find all attraction bookings
-    const result4 = await prisma.attraction_Booking.findMany();
+  // find all attraction bookings
+  const attraction = await prisma.attraction_Booking.findMany();
 
-    return { result, result2, result3, result4 };
+  return { hotel, security, car, attraction };
 };
 
 // get single contract
