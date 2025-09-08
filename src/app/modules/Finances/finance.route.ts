b4d4f3388904abd672d884ad7f1b14e929552347
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-09 00:25:08 +0600

    add finance modules

--- src/app/modules/Finances/finance.route.ts
+++ src/app/modules/Finances/finance.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { FinanceController } from "./finance.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
 // get all service providers finances
-router.get("/", FinanceController.getAllProvidersFinances);
+router.get("/", auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), FinanceController.getAllProvidersFinances);
 
 export const financeRoutes = router;
