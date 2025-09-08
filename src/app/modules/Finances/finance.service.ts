8b006d427e566f1a0df6f57d389c780a277c1a05
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-09 00:33:21 +0600

    get all finance

--- src/app/modules/Finances/finance.service.ts
+++ src/app/modules/Finances/finance.service.ts
@@ -2,10 +2,8 @@ import prisma from "../../../shared/prisma";
 
 // get all service providers finances
 const getAllProvidersFinances = async () => {
-    const result = await prisma.payment.findMany({
-       
-    });
-    return result;
+  const result = await prisma.payment.findMany({});
+  return result;
 };
 
 export const FinanceService = {
