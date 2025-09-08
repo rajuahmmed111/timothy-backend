30e564d4e8bae7c7bf63f81029e3df8a346d1fa7
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-09 00:48:33 +0600

    working filter and search in finance

--- src/app/modules/Finances/finance.controller.ts
+++ src/app/modules/Finances/finance.controller.ts
@@ -3,11 +3,16 @@ import catchAsync from "../../../shared/catchAsync";
 import { FinanceService } from "./finance.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { pick } from "../../../shared/pick";
+import { filterField } from "./finance.constant";
+import { paginationFields } from "../../../constants/pagination";
 
 // get all service providers finances
 const getAllProvidersFinances = catchAsync(
   async (req: Request, res: Response) => {
-    const result = await FinanceService.getAllProvidersFinances();
+      const filter = pick(req.query, filterField);
+      const options = pick(req.query, paginationFields);
+    const result = await FinanceService.getAllProvidersFinances(filter, options);
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
--- src/app/modules/Finances/finance.service.ts
+++ src/app/modules/Finances/finance.service.ts
@@ -1,7 +1,20 @@
+import { Prisma } from "@prisma/client";
+import { paginationHelpers } from "../../../helpars/paginationHelper";
+import { IPaginationOptions } from "../../../interfaces/paginations";
 import prisma from "../../../shared/prisma";
+import { IFilterRequest } from "./finance.interface";
 
 // get all service providers finances
-const getAllProvidersFinances = async () => {
+const getAllProvidersFinances = async (
+  params: IFilterRequest,
+  options: IPaginationOptions
+) => {
+  const { limit, page, skip } = paginationHelpers.calculatedPagination(options);
+
+  const { searchTerm, timeRange, ...filterData } = params;
+
+  const filters: Prisma.PaymentWhereInput[] = [];
+
   const result = await prisma.payment.findMany({});
   return result;
 };
