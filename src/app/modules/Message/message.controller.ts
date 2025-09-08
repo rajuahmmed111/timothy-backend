83d470debf13133b57c369bfe19aede93c01570e
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-08 22:04:27 +0600

    send message with files

--- src/app/modules/Message/message.controller.ts
+++ src/app/modules/Message/message.controller.ts
@@ -1,31 +1,34 @@
-import { Request, Response } from 'express';
-import catchAsync from '../../../shared/catchAsync';
-import { messageServices } from './message.service';
-import sendResponse from '../../../shared/sendResponse';
-import httpStatus from 'http-status';
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import { messageServices } from "./message.service";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import { uploadFile } from "../../../helpars/fileUploader";
 
 // send message
 const sendMessage = catchAsync(async (req: Request, res: Response) => {
   const senderId = req.user?.id;
   const receiverId = req.params.receiverId;
   const { message } = req.body;
-  // console.log(senderId, "senderId")
 
-  const files = req.files as
-    | { [fieldname: string]: Express.Multer.File[] }
-    | undefined;
+  // multer array => req.files as Express.Multer.File[]
+  const files = req.files as Express.Multer.File[] | undefined;
+
   let imageUrls: string[] = [];
-  if (files && files.messageImages) {
-    imageUrls = files.messageImages.map((file: Express.Multer.File) => {
-      const fileUrl = `${req.protocol}://${req.get('host')}/uploads/messages/${
-        file.filename
-      }`;
-      return fileUrl;
-    });
+
+  if (files && files.length > 0) {
+    // Cloudinary upload use
+    const uploadResults = await Promise.all(
+      files.map(async (file) => {
+        const uploaded = await uploadFile.uploadToCloudinary(file);
+        return uploaded?.secure_url; // cloudinary URL
+      })
+    );
+    imageUrls = uploadResults.filter((url): url is string => !!url);
   }
 
   const result = await messageServices.sendMessage(
-    senderId,
+    senderId!,
     receiverId,
     message,
     imageUrls
@@ -34,7 +37,7 @@ const sendMessage = catchAsync(async (req: Request, res: Response) => {
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
     success: true,
-    message: 'Message send successfully',
+    message: "Message send successfully",
     data: result,
   });
 });
@@ -47,43 +50,43 @@ const getMyChannel = catchAsync(async (req: Request, res: Response) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Channel retrieved successfully',
+    message: "Channel retrieved successfully",
     data: result,
   });
 });
 
 // get all messages
 const getMessagesFromDB = catchAsync(async (req: Request, res: Response) => {
- const {channelName} = req.params
-//  console.log(channelName);
- 
+  const { channelName } = req.params;
+  //  console.log(channelName);
+
   const messages = await messageServices.getMessagesFromDB(channelName);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Messages retrieved successfully',
+    message: "Messages retrieved successfully",
     data: messages,
   });
 });
 
 const getUserChannels = catchAsync(async (req: Request, res: Response) => {
-  const userId = req.user.id
+  const userId = req.user.id;
   // console.log(userId);
-  
-   const channels = await messageServices.getUserChannels(userId);
- 
-   sendResponse(res, {
-     statusCode: httpStatus.OK,
-     success: true,
-     message: 'Channels retrieved successfully',
-     data: channels,
-   });
- });
+
+  const channels = await messageServices.getUserChannels(userId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Channels retrieved successfully",
+    data: channels,
+  });
+});
 
 export const messageControllers = {
   sendMessage,
   getMyChannel,
   getMessagesFromDB,
-  getUserChannels
+  getUserChannels,
 };
