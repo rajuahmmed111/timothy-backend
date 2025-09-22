8af3aff8ecbf1a8426f84ea923f17bad77aea93b
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-23 02:58:49 +0600

    payment intent

--- src/app/modules/Payment/payment.sercice.ts
+++ src/app/modules/Payment/payment.sercice.ts
@@ -387,12 +387,15 @@ const stripeHandleWebhook = async (event: Stripe.Event) => {
   switch (event.type) {
     case "payment_intent.succeeded": {
       const paymentIntent = event.data.object as Stripe.PaymentIntent;
+      console.log(`Payment intent succeeded: ${paymentIntent}`);
       const paymentIntentId = paymentIntent.id;
+      console.log(`Payment intent ID: ${paymentIntentId}`);
 
       // find Payment
       const payment = await prisma.payment.findFirst({
         where: { payment_intent: paymentIntentId },
       });
+      console.log(payment, "payment");
       if (!payment) {
         console.log(`No payment found for payment intent: ${paymentIntentId}`);
         break;
@@ -404,6 +407,7 @@ const stripeHandleWebhook = async (event: Stripe.Event) => {
         const applicationFee = paymentIntent.application_fee_amount ?? 0;
         providerReceived = amountReceived - applicationFee;
       }
+      console.log(providerReceived, "providerReceived");
 
       // update Payment to PAID
       await prisma.payment.update({
