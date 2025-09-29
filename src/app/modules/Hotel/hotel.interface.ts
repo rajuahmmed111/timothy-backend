1477b96a7c0e9d2347d0cd50c6d76ecc602a3c6f
Raju Ahmmed <ahraju2003@gmail.com>
2025-09-29 21:23:58 +0600

    updating hotel and room

--- src/app/modules/Hotel/hotel.interface.ts
+++ src/app/modules/Hotel/hotel.interface.ts
@@ -10,4 +10,17 @@ export type IHotelFilterRequest = {
   maxPrice?: number | undefined;
   fromDate?: string | undefined;
   toDate?: string | undefined;
+  hotelBreakfast?: boolean | undefined;
+  hotelKitchen?: boolean | undefined;
+  hoitelWifi?: boolean | undefined;
+  hotelParking?: boolean | undefined;
+  
+  hotelAC?: boolean | undefined;
+  hotelPool?: boolean | undefined;
+  hotelSmoking?: boolean | undefined;
+  hotelTv?: boolean | undefined;
+  hotelWashing?: boolean | undefined;
+  hotelNumAdults?: number | undefined;
+  hotelNumChildren?: number | undefined;
+  hotelNumberOfRooms?: number | undefined;
 };
